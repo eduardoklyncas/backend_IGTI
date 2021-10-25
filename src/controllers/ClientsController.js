a53872f3f37c2732a2691eee1740c8092210bb23
Eduardo Kuchenbecker <eduardo.k@lyncas.net>
2021-10-24 22:27:46 -0300

    adicionar rota de update e getClientById

--- src/controllers/ClientsController.js
+++ src/controllers/ClientsController.js
@@ -1,4 +1,5 @@
 const Clients = require('../models/Clients');
+const { update } = require('../models/User');
 const User = require('../models/User');
 
 module.exports = {
@@ -16,6 +17,40 @@ module.exports = {
         const clients = await Clients.findAll({where: {company_id: company_id}});
 
         return res.json(clients);
+    },
+
+    async getById(req, res) {
+        const { company_id, item_id } = req.params; 
+        const client = await Clients.findOne({where: {id: item_id}});
+
+        return res.json(client)
+    },
+
+    async update(req, res) {
+        const { company_id, item_id } = req.params;  
+        const { name, email, telefone, rua, numero, bairro, complemento, cidade, estado } = req.body;
+
+        const user = await User.findByPk(company_id);
+
+        if (!user) {
+            return res.status(400).json({ error: 'Usuário não encontrado' });
+        }
+
+        const client = await Clients.update({
+            name,
+            email,
+            telefone,
+            rua,
+            numero,
+            bairro,
+            complemento,
+            cidade,
+            estado,
+            company_id,
+        }, { where: { id: item_id } });
+
+        return res.json(client);
+
     },
 
     async store(req, res) {
--- src/routes.js
+++ src/routes.js
@@ -7,11 +7,10 @@ const routes = express.Router();
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
 routes.post('/users/login', UserController.login);
-// criar rota de login
-
-routes.get('/users/:company_id/clients', ClientsController.index);
+routes.get('/users/:company_id/:item_id', ClientsController.getById);
+routes.get('/users/:company_id/clients/all', ClientsController.index);
+routes.put('/users/:company_id/editar/:item_id', ClientsController.update);
 routes.post('/users/:company_id/clients', ClientsController.store);
 routes.delete('/users/:company_id', ClientsController.delete);
-//criar rota para edição de cliente
 
 module.exports = routes;
